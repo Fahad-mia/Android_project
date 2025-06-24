6677c2624d5c2184fd45ec70e42d4ff468c43684
Fahad-mia <fahadcse838@gmail.com>
2025-06-25 03:05:51 +0600

    push with error

--- Android_project(Java)/app/src/main/java/com/example/pushnotificationdemo/sendnotification/RetrofitClient.java
+++ Android_project(Java)/app/src/main/java/com/example/pushnotificationdemo/sendnotification/RetrofitClient.java
@@ -1,15 +1,21 @@
 package com.example.pushnotificationdemo.sendnotification;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit = null;
 
+
     public static Retrofit getInstance(String serverKey) {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY); // or BASIC/HEADERS
+
         OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(new AuthInterceptor(serverKey))
+                .addInterceptor(loggingInterceptor) // 👈 added here
                 .build();
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
